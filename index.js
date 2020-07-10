41018a8a3f12714338716eb25cf6f3ddafcfdda1
Sean Graham <grahams@sean-graham.com>
2020-07-10 14:00:22 -0400

    basic basic shit

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const SlackBot = require('slackbots');
 const axios = require('axios')
 const dotenv = require('dotenv')
+const fs = require('fs');
+
+const lines = fs.readFileSync('better-fates.txt').toString().split("\n")
 
 dotenv.config()
 
@@ -9,24 +12,37 @@ const bot = new SlackBot({
     name: 'Fate Wheel'
 })
 
-
-bot.on('start', () => {
-    const params = {
-        icon_emoji: ':robot_face:'
-    }
-
-    bot.postMessageToChannel(
-        'workshop',
-        'Mothafuckin @fatewheel',
-        params
-    );
+// Error Handler
+bot.on('error', (err) => {
+    console.log(err);
 })
 
-function handleMessage(message) {
-    bot.postMessageToChannel(
-        'workshop',
-        'Responding',
-        params
-    );
+// Message Handler
+bot.on('message', (data) => {
+    // Only read messages that are not other bot messages
+    if( (data.subtype && (data.subtype === 'bot_message' || data.subtype === 'message_replied')) || (data.message && data.data.subtype && data.data.subtype === 'bot_message')) {
+		return
+    }
+
+    if(data.type !== 'message') {
+        return;
+    }
+
+    handleMessage(data.text, data.channel);
+
+	return;
+})
+
+function handleMessage(message, channel) {
+	if(message.startsWith(`<@${bot.self.id}> `) ) {
+		var random = Math.floor(Math.random() * lines.length);
+
+		const params = {
+			icon_emoji: ':robot_face:'
+		}
+
+
+		bot.postMessage(channel, lines[random], params);
+	}
 }
 
