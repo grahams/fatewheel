56829846e2cc33d02941733f974d82fd42e3f044
Sean Graham <github@sean-graham.com>
2020-08-17 15:25:55 -0400

    fixed the command regex so it won't puke on spaces

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ bot.on('message', (data) => {
 })
 
 function handleMessage(data) {
-    const re = new RegExp(/<@(.*)> (\S+) ?(.*)?/);
+    const re = new RegExp(/<@(.*)>\s+(\S+)\s+(.*)?/);
 
     if(data.text === undefined) {
         return;
